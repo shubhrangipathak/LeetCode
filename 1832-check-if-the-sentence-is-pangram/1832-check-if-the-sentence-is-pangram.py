a392ea45db2e157bffc75500d7ae9b7f06306267
shubhipathak11 <90844602+shubhipathak11@users.noreply.github.com>
2022-10-17 12:09:01 +0530

    Time: 51 ms (65.02%), Space: 13.8 MB (95.55%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -7,8 +7,6 @@ class Solution:
             for i in sentence:
                 if i not in d:
                     d[i]=1 
-                else:
-                    d[i]+=1
             if len(d)==26:
                 return True
             else:
