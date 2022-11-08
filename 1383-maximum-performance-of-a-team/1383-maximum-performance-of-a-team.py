032df16b3ca7f6d544a94d23b1de989770599d8a
shubhipathak11 <90844602+shubhipathak11@users.noreply.github.com>
2022-11-09 00:39:22 +0530

    Time: 1535 ms (5.00%), Space: 35.3 MB (10.29%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        s=[list(i) for i in zip(efficiency,speed)]
+        s.sort(reverse=True)
+        print(s)
+        min_heap=[]
+        high_speed=0
+        res=0
+        for ef,sp in s:
+            if len(min_heap)==k:
+                high_speed-=heappop(min_heap)
+            high_speed+=sp
+            heappush(min_heap,sp)
+            res=max(res,high_speed*ef)
+            # print(min_heap)
+        
+        return res%(10**9+7)
