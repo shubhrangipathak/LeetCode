723c9d6bb219cad1aca82ed1ddf61ad05d3d3063
Shubhrangi Pathak <90844602+shubhipathak11@users.noreply.github.com>
2023-04-26 00:50:26 +0530

    Update 0041-first-missing-positive.py

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -6,3 +6,21 @@ class Solution:
             if i not in v:
                 return i
             i += 1
+            
+#METHOD 2
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        d = {}
+        
+        for i in nums:
+            d[i] = d.get(i,0)+1
+        #Accessing values from a dictionary takes O(1) time.
+        
+        val = 1
+        
+        while 1:
+            
+            if val not in d:
+                return val
+            
+            val += 1
