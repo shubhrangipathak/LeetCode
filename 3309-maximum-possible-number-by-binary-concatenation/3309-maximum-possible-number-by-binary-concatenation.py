041c667710056e254046fa2f9219674f16f5f12a
Shubhrangi Pathak <90844602+shubhrangipathak@users.noreply.github.com>
2024-10-06 23:58:06 +0530

    Time: 38 ms (100.00%), Space: 16.7 MB (100.00%) - LeetHub

--- 3309-maximum-possible-number-by-binary-concatenation/3309-maximum-possible-number-by-binary-concatenation.py
+++ 3309-maximum-possible-number-by-binary-concatenation/3309-maximum-possible-number-by-binary-concatenation.py
@@ -2,7 +2,7 @@ class Solution:
     def maxGoodNumber(self, nums: List[int]) -> int:
         binaries = [bin(num)[2:] for num in nums]
 
-        def concat_binaries(a: str, b: str, c: str) -> int:
+        def concat_binaries(a,b,c):
             return int(a + b + c, 2)
 
-        return max(concat_binaries(a, b, c) for a, b, c in permutations(binaries, 3))
+        return max(concat_binaries(a, b, c) for a, b, c in permutations(binaries))
